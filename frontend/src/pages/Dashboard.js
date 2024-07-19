9174fab5be8b2ed9561f4d3035ee50f682eb9304
dizvyagintsev <dizvyagintsev@gmail.com>
2024-07-19 09:28:23 +0200

    save filters when page is reloaded

--- frontend/src/pages/Dashboard.js
+++ frontend/src/pages/Dashboard.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, {useEffect, useState} from "react";
 import { Helmet } from "react-helmet-async";
 import { Container, Typography, Box, Grid, Card } from "@mui/material";
 import { useSettingsContext } from "../components/settings";
@@ -11,19 +11,26 @@ import MetricsDataGrid from "../components/metrics-data-grid/MetricsDataGrid";
 
 export default function Dashboard() {
   const { themeStretch } = useSettingsContext();
-  const [selectedEvent, setSelectedEvent] = useState(Event.ORDER_COMPLETED);
-  const [dateRange, setDateRange] = useState([
-    dayjs("2024-05-15"),
-    dayjs("2024-07-05"),
-  ]);
+
+  const initialEvent = localStorage.getItem('selectedEvent') || Event.ORDER_COMPLETED;
+  const initialDateRange = JSON.parse(localStorage.getItem('dateRange')) || ['2024-05-15', '2024-07-05']
+
+  const [selectedEvent, setSelectedEvent] = useState(initialEvent);
+  const [dateRange, setDateRange] = useState([dayjs(initialDateRange[0]), dayjs(initialDateRange[1])]);
+
+
+  useEffect(() => {
+    localStorage.setItem('selectedEvent', selectedEvent);
+  }, [selectedEvent]);
+
+  useEffect(() => {
+    localStorage.setItem('dateRange', JSON.stringify(dateRange.map(date => date.toISOString())));
+  }, [dateRange]);
 
   const handleEventChange = (event) => {
     setSelectedEvent(event.target.value);
   };
 
-  const handleDateRangeChange = (newValue) => {
-    setDateRange(newValue);
-  };
 
   return (
     <>
@@ -35,17 +42,20 @@ export default function Dashboard() {
         <Typography variant="h3" component="h1" paragraph>
           Dashboard
         </Typography>
+
         <Box sx={{ mb: 3 }}>
           <Filters
             selectedEvent={selectedEvent}
             dateRange={dateRange}
             onEventChange={handleEventChange}
-            onDateRangeChange={handleDateRangeChange}
+            onDateRangeChange={setDateRange}
           />
         </Box>
+
         <Card>
           <MetricsDataGrid event={selectedEvent} dateRange={dateRange} />
         </Card>
+
         <Box sx={{ mt: 3 }}>
           <Grid container spacing={2}>
             <Grid item xs={12} md={6}>
@@ -53,6 +63,7 @@ export default function Dashboard() {
                 <LineChartComponent />
               </Card>
             </Grid>
+
             <Grid item xs={12} md={6}>
               <Card>
                 <BubbleChartComponent />
@@ -60,6 +71,7 @@ export default function Dashboard() {
             </Grid>
           </Grid>
         </Box>
+
       </Container>
     </>
   );
